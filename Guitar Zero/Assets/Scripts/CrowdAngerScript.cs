b0a3b3fa4ed4f762086dba759c3a72762ca294b2
tsarachan <tes353@nyu.edu>
2016-09-23 14:34:32 -0400

    Added timer
    Added timer for crowd anger.

---
+++ Guitar Zero/Assets/Scripts/CrowdAngerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CrowdAngerScript : MonoBehaviour {
+
+	private Image meter;
+	public float angerDelay = 2.0f; //total amount of time it takes for the meter to reach zero
+	private float angerTimer = 2.0f;
+
+	private void Start(){
+		meter = GetComponent<Image>();
+	}
+
+	private void Update(){
+		angerTimer -= Time.deltaTime;
+
+		meter.fillAmount = angerTimer/angerDelay;
+
+		if (angerTimer <= 0.0f){
+			angerTimer = angerDelay;
+		}
+	}
+}
