10a600e899e35d4822dedf254780b2639763bdbd
tsarachan <tes353@nyu.edu>
2016-09-26 07:40:20 -0400

    Implemented losing health
    Player now loses health when the crowd gets angry while the crowd multiplier is > 1.

--- Guitar Zero/Assets/Scripts/CrowdAngerScript.cs
+++ Guitar Zero/Assets/Scripts/CrowdAngerScript.cs
@@ -14,6 +14,8 @@ public class CrowdAngerScript : MonoBehaviour {
 	private const string GAMEMANAGER = "GameManager";
 	private ScoreManager scoreManager;
 	private const string SCORE_NAME = "Score";
+	private HealthManager healthManager;
+	private const string LIFE_ICONS = "Life icons";
 
 	private void Start(){
 		meter = GetComponent<Image>();
@@ -21,6 +23,7 @@ public class CrowdAngerScript : MonoBehaviour {
 		angerTimer = angerDelay;
 		matchManagerScript = transform.root.Find(GAMEMANAGER).GetComponent<MatchManagerScript>();
 		scoreManager = transform.parent.Find(SCORE_NAME).GetComponent<ScoreManager>();
+		healthManager = transform.parent.Find(LIFE_ICONS).GetComponent<HealthManager>();
 	}
 
 	private void Update(){
@@ -33,16 +36,23 @@ public class CrowdAngerScript : MonoBehaviour {
 		}
 	}
 
+	/// <summary>
+	/// Refill the crowd's patience when the player successfully makes a match before time runs out.
+	/// </summary>
 	public void ResetForSuccess(){
 		angerDelay *= delayReduction;
 		if (angerDelay < MIN_DELAY) { angerDelay = MIN_DELAY; }
 		angerTimer = angerDelay;
 	}
 
+	/// <summary>
+	/// If the crowd's patience runs out, reset their patience and cause ancillary effects.
+	/// </summary>
 	public void ResetForFailure(){
 		angerDelay = maxDelay;
 		angerTimer = angerDelay;
-		matchManagerScript.CrowdMultiplier = matchManagerScript.Min_Crowd_Multiplier;
+		healthManager.DetermineHealthEffect(matchManagerScript.CrowdMultiplier); //possibly lose health
+		matchManagerScript.CrowdMultiplier = matchManagerScript.Min_Crowd_Multiplier; //reset the multiplier to 1
 		scoreManager.BonusFeedback(matchManagerScript.Min_Crowd_Multiplier);
 	}
 }
---
+++ Guitar Zero/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthManager : MonoBehaviour {
+
+	private Image[] hearts;
+	private MatchManagerScript matchManager;
+	private const string GAME_MANAGER = "GameManager";
+
+	private void Start(){
+		hearts = new Image[transform.childCount];
+		hearts = FindHearts();
+		matchManager = transform.root.Find(GAME_MANAGER).GetComponent<MatchManagerScript>();
+	}
+
+	/// <summary>
+	/// Get an array of the hearts used to measure life.
+	/// </summary>
+	/// <returns>An array containing all the hearts.</returns>
+	private Image[] FindHearts(){
+
+		Image[] temp = new Image[transform.childCount];
+
+		for (int i = 0; i < transform.childCount; i++){
+			temp[i] = transform.GetChild(i).GetComponent<Image>();
+		}
+
+		return temp;
+	}
+
+	/// <summary>
+	/// Lose 1 health if this is called when the crowd multiplier is > 1. This function is called from
+	/// CrowdAngerScript, which knows when the crowd's patience runs out.
+	/// </summary>
+	/// <param name="multiplier">The crowd multiplier.</param>
+	public void DetermineHealthEffect(int multiplier){
+		if (multiplier > matchManager.Min_Crowd_Multiplier){
+			foreach (Image heart in hearts){
+				if (heart.enabled){
+					heart.enabled = false;
+					break;
+				}
+			}
+		}
+	}
+}
