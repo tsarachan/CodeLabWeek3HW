863bae42acade9bbe4b394011dbda73a8f3d9f14
tsarachan <tes353@nyu.edu>
2016-09-27 12:00:49 -0400

    Added credits scene

---
+++ Guitar Zero/Assets/Scripts/CreditSceneLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class CreditSceneLoader : MonoBehaviour {
+
+	private const string TITLE_SCENE_NAME = "Title scene";
+
+	public void LoadTitleScene(){
+		SceneManager.LoadScene(TITLE_SCENE_NAME);
+	}
+}
