4f6507feced6212a31910b34de3dffa79249aef3
tsarachan <tes353@nyu.edu>
2016-09-27 12:03:25 -0400

    Credits scene accessible
    Credits scene can now be accessed from the title scene.

--- Guitar Zero/Assets/Scripts/SceneChanger.cs
+++ Guitar Zero/Assets/Scripts/SceneChanger.cs
@@ -4,12 +4,14 @@ using System.Collections;
 
 public class SceneChanger : MonoBehaviour {
 
-	const string TITLE_SCENE = "Title scene";
-	const string GAME_SCENE = "GameScene";
-
-	void Update(){
+	private const string TITLE_SCENE = "Title scene";
+	private const string GAME_SCENE = "GameScene";
+	private const string CREDITS_SCENE = "Credits scene";
+	private void Update(){
 		if (Input.GetMouseButtonDown(0)){
 			SceneManager.LoadScene(GAME_SCENE);
+		} else if (Input.GetMouseButtonDown(1)){
+			SceneManager.LoadScene(CREDITS_SCENE);
 		}
 	}
 }
